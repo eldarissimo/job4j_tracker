2376c566b2c88ebc4fe6545a12f1a7c0a6a34ea7
eldarissimo <eishmuratov@yandex.ru>
2023-01-24 01:12:22 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -31,15 +31,28 @@ public class Tracker {
         return Arrays.copyOf(rsl, counter);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean result = indexOf(id) != -1;
+        if (result) {
+            item.setId(id);
+            items[indexOf(id)] = item;
+        }
+        return result;
     }
 }
