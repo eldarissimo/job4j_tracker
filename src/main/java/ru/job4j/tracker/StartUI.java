0f0aa102d757c9a1340fe813bbfc000177e9669f
eldarissimo <eishmuratov@yandex.ru>
2023-01-24 20:14:28 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(dateTimeFormatter));
+        System.out.println(item);
     }
 }
