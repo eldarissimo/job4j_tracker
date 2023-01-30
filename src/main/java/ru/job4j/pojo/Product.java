4d010d5630877424fcf280a9f125f27dbe4099d2
eldarissimo <eishmuratov@yandex.ru>
2023-01-30 23:35:56 +0300

    пустой

--- src/main/java/ru/job4j/pojo/Product.java
+++ src/main/java/ru/job4j/pojo/Product.java
@@ -1,6 +1,25 @@
 package ru.job4j.pojo;
 
+import java.util.Objects;
+
 public class Product {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return count == product.count && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, count);
+    }
+
     private String name;
     private int count;
 
